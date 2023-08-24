ede513617a90c7002a01817b05037beef5b7027a
Dababung <127767657+Dababung@users.noreply.github.com>
2023-08-24 23:39:44 +0300

    Add files via upload
    readable printing

--- JsonPrinter.java
+++ JsonPrinter.java
@@ -1,5 +1,36 @@
 package bg.tu_varna.sit.b1.f21621618;
 
 public class JsonPrinter {
-    public void print(JsonData jsonData){}
+    public void print(StringBuilder jsonData){
+        int level=0;
+        boolean commas=false;
+        boolean newLine=false;
+        for (int i=0;i<jsonData.length();i++){
+            char curr=jsonData.charAt(i);
+            if (curr=='"')
+                commas=!commas;
+
+        if (!commas){
+            if (curr=='{'||curr=='['){
+                level++;
+            }
+            else if(curr=='}'||curr==']'){
+                level--;
+            }
+            else if (curr!=','){
+                newLine=false;
+            }
+        }
+        if (newLine){
+            System.out.println();
+            for (int j=0;j<level;j++){
+                System.out.println(" ");
+            }
+            newLine=false;
+        }
+        System.out.print(curr);
+        }
+        System.out.println();
+    }
+
 }
